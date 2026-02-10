2ca57a5b885c9fbdff478a7d29b903b6518959c3
Christin David Bose <chris241@purdue.edu>
2026-02-09 21:58:24 -0500

    env files

---
+++ env.sh
@@ -0,0 +1,4 @@
+export CUDA_INSTALL_PATH=/usr/local/cuda-12.9
+export CUDA_PATH=/usr/local/cuda-12.9
+export LD_LIBRARY_PATH=/usr/local/cuda-12.9/targets/x86_64-linux/lib/:/usr/lib/x86_64-linux-gnu/:$LD_LIBRARY_PATH
+export PATH=$CUDA_INSTALL_PATH/bin:$PATH
